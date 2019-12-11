2a2a3cc9373c27863250ccc3643481e8ed217126
Blake Williams <michaelbwilliams100@gmail.com>
2019-12-11 18:35:52 -0500

    Check that fields are present

--- server/validation/validateSightingInsert.ts
+++ server/validation/validateSightingInsert.ts
@@ -4,9 +4,12 @@ const validateSightingInsert = (data: any) => {
     console.log(data)
     const errors: any = {};
     data.name = !isEmpty(data.name) ? data.name : '';
+    data.person = !isEmpty(data.person) ? data.person : '';
+    data.location = !isEmpty(data.location) ? data.location : '';
+    data.date = !isEmpty(data.date) ? data.date : '';
     // data.column = !isEmpty(data.column) ? data.column : '';
 
-    if (isEmpty(data.name)) {
+    if (isEmpty(data.name) || isEmpty(data.person) || isEmpty(data.location) || isEmpty(data.date)) {
         errors.name = 'Cannot have an empty comname.';
     }
 
