5ece3562d989ab57dfc9cf8f064bc28f05d0914b
Blake Williams <michaelbwilliams100@gmail.com>
2019-12-12 16:38:40 -0500

    More updates

--- client/src/components/Home/NewSightingsModal.js
+++ client/src/components/Home/NewSightingsModal.js
@@ -2,54 +2,8 @@ import React, {useState, useEffect} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form"
-import axios from 'axios';
 
 const NewSightingsModal = (props) => {
-  const [flower, setFlower] = useState('');
-  const [member, setMember] = useState('');
-  const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // var formData = new FormData();
-    // formData.append('name', flower);
-    // formData.append('person', member);
-    // formData.append('location', location);
-    // formData.append('sighted', date);
-    console.log(flower + ' ' + member + ' ' + location + ' ' + date);
-    // axios({
-    //   method: 'post',
-    //   url: '/api/flowers/insert',
-    //   data: {
-    //     name: flower,
-    //     person: member,
-    //     location: location,
-    //     date: date
-    //   },
-    //   headers: {
-    //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    //   },
-    // });
-    axios.post('/api/flowers/insert', {flower, member, location, date})
-    .then((res) => {
-      console.log("Heres the response: " + res.status);
-    }).catch((err) => {
-      console.log("Oops something not right boi\n");
-      console.log(err);
-    })
-   
-    // axios.post('/user', {
-    //   firstName: 'Fred',
-    //   lastName: 'Flintstone'
-    // })
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-}
   return (
         <>
             <Modal show={props.show} onHide={props.handleClose} className='modal'>
@@ -57,27 +11,27 @@ const NewSightingsModal = (props) => {
                   <Modal.Title>A New Sighting Appears!</Modal.Title>
                 </Modal.Header>
 
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={props.handleSubmit}>
                   <Modal.Body className='m-auto'>
 
                     <Form.Group controlId="flower">
                       <Form.Label className="form-label">Flower Sighted: </Form.Label>
-                      <Form.Control type="text" placeholder="Large text" value={flower} onChange={event => setFlower(event.target.value)}/>
+                      <Form.Control type="text" placeholder="Large text" required={true} value={props.flower} onChange={event => props.setFlower(event.target.value)}/>
                     </Form.Group>
                     <br />
                     <Form.Group controlId="person">
                       <Form.Label className="form-label">Member Name: </Form.Label>
-                      <Form.Control type="text" placeholder="Large text" value={member} onChange={event => setMember(event.target.value)}/>
+                      <Form.Control type="text" placeholder="Large text" required={true} value={props.member} onChange={event => props.setMember(event.target.value)}/>
                     </Form.Group>
                     <br />
                     <Form.Group controlId="location">
                       <Form.Label className="form-label">Location: </Form.Label>
-                      <Form.Control type="text" placeholder="Large text" value={location} onChange={event => setLocation(event.target.value)}/>
+                      <Form.Control type="text" placeholder="Large text" required={true} value={props.location} onChange={event => props.setLocation(event.target.value)}/>
                     </Form.Group>
                     <br />
                     <Form.Group controlId="date">
                       <Form.Label className="form-label">Date: </Form.Label>
-                      <Form.Control type="date" value={date} onChange={event => setDate(event.target.value)}/>
+                      <Form.Control type="date" required={true} value={props.date} onChange={event => props.setDate(event.target.value)}/>
                     </Form.Group>
 
                   </Modal.Body>
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -4,6 +4,7 @@ import FlowerGrid from '../../components/Home/Images/FlowerGrid';
 import ImageFilter from "../../components/Home/Images/ImageFilter";
 import Button from "react-bootstrap/Button";
 import NewSightingsModal from "../../components/Home/NewSightingsModal";
+import axios from 'axios';
 
 
 const Home = (props) => {
@@ -13,13 +14,37 @@ const Home = (props) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [flower, setFlower] = useState('');
+    const [member, setMember] = useState('');
+    const [location, setLocation] = useState('');
+    const [date, setDate] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(flower + ' ' + member + ' ' + location + ' ' + date);
+        axios.post('/api/flowers/insert', {flower, member, location, date})
+        .then((res) => {
+          if(res.status == 200){
+            alert("New Sighting Created :)");
+          }
+          setShow(false)
+        })
+    }
     
 
     return (
         <div>
             <h2 className='header-text'>Select a flower!</h2>
-            <Button variant="primary" onClick={handleShow}>Hello!</Button>
-            <NewSightingsModal show={show} handleClose={handleClose}/>
+            <Button variant="secondary" onClick={handleShow}>Hello!</Button>
+            <NewSightingsModal 
+                show={show} 
+                handleClose={handleClose} 
+                handleSubmit={handleSubmit}
+                flower={flower} setFlower={setFlower}
+                member={member} setMember={setMember}
+                location={location} setLocation={setLocation}
+                date={date} setDate={setDate}
+            />
             <ImageFilter input={input} setInput={setInput}/>
             <FlowerGrid filter={input} imgs={imgs}/>
         </div>
